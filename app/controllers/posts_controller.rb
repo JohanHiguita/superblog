75f9ac0f8751ee98179ca60c13d3066d442060b8
Johan Higuita <higuitajohan@gmail.com>
2018-12-05 09:35:26 -0500

    Metodo create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,32 +18,32 @@ class PostsController < ApplicationController
   end
 
   def create
-    # @post = Post.new(post_params)
-
-
-    #   if @post.save
-        
-    #   else
-        
-    #   end
-
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+     flash[:notice]= "El Post ha sido creado exitosamente"
+     redirect_to posts_path 
+    else
+      flash[:alert] = "Error al crear el post"
+      render :new
+    end
   end
 
-  def update
+def update
 
       # if @post.update(post_params)
-     
+
       # else
-        
+
       # end
 
-  end
+    end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     #@post.destroy
-   
+
   end
 
   private
@@ -56,4 +56,4 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :content, :user_id)
     end
-end
+  end
