d7675d8d8748206716b2c52e46ef9eebe453af64
Johan Higuita <higuitajohan@gmail.com>
2018-12-05 18:01:41 -0500

    Crea modelo comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
-  devise_for :users
-  
+	root to: "posts#index"
+	devise_for :users
 
-  root to: "posts#index"
+	resources :posts do
+		resources :comments, only: [:create]
+	end
+
+
+
 end
